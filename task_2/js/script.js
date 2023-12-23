4b39d35315089c7bba16209adb6fbe011face19b
bhagyaunnikrishnan <bhagyanandhu2002@gmail.com>
2023-12-23 16:57:10 +0530

    created about page

--- task_2/js/script.js
+++ task_2/js/script.js
@@ -1,7 +1,5 @@
-var typed = new Typed(".multiple-text", {
-    strings: ["Web Developer!","UI Developer","Python Developer","Student."],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
+
+window.addEventListener("scroll", function() {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
