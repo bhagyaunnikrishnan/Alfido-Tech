048e8176a43bfe3268b8f088c489c8f209839391
bhagyaunnikrishnan <bhagyanandhu2002@gmail.com>
2023-12-24 21:17:44 +0530

    updated

--- task_2/js/script.js
+++ task_2/js/script.js
@@ -1,3 +1,10 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Web Developer!","Student."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
 
 window.addEventListener("scroll", function() {
     const header = document.querySelector("header");
