eae91dbf4e4903a4e87e2163e6a358cbbf99a6eb
bhagyaunnikrishnan <bhagyanandhu2002@gmail.com>
2023-12-25 14:46:43 +0530

    service page created

--- task_2/js/script.js
+++ task_2/js/script.js
@@ -10,3 +10,25 @@ window.addEventListener("scroll", function() {
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+const serviceModals = document.querySelectorAll(".service-modal");
+const readmoreBtns = document.querySelectorAll(".read-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function(modalClick) {
+    serviceModals[modalClick].classList.add("active");
+}
+
+readmoreBtns.forEach((readmoreBtn, i) =>{
+    readmoreBtn.addEventListener("click", () => {
+        modal(i);
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
