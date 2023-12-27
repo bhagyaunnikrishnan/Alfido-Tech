18a016927dc8a8ce09b1898fc04e4fdb53e5e033
bhagyaunnikrishnan <bhagyanandhu2002@gmail.com>
2023-12-27 23:02:31 +0530

    added media query

--- task_2/js/script.js
+++ task_2/js/script.js
@@ -132,3 +132,33 @@ if(savedTheme) {
     document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
     themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
 }
+
+//Navigation 
+
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a");
+
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
+
+navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+        navigation.classList.remove("active");
+    });
+});
+
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 100
+});
+
+ScrollReveal().reveal('.home .info h2', { delay: 500, origin: 'left'});
